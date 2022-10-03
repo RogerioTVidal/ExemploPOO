1296049bfb1a53684700b88a167d03684cbf5ae0
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2022-10-03 17:05:21 -0300

    Initial commit

---
+++ Models/Professor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class Professor: Pessoa
+    {
+        public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, eu sou o professor {Nome} e meu salário é de {Salario}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ExemploPOO.Models;
+
+
+
+Corrente c1 = new Corrente();
+
+c1.Creditar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+// Aluno a1 = new Aluno();
+
+// a1.Nome = "Rogério";
+// a1.Idade = 55;
+// a1.Email = "teste@teste.com";
+// a1.Nota = 10;
+
+// a1.Apresentar();
+
+// Professor p1 = new Professor();
+
+// p1.Nome = "Rose";
+// p1.Idade = 55;
+// p1.Salario = 1000;
+
+// p1.Apresentar();
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+
+// p1.Nome = "Rogério";
+// p1.Idade = 55;
+
+// p1.Apresentar();
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000M);
+
+// c1.ExibirSaldo();
+// c1.Sacar(10000);
+// c1.ExibirSaldo();
